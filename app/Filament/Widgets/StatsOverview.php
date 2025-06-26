4fee9b71eeda19eec369ca57039d6230108a8938
Drenzzz <realdrenzzz@gmail.com>
2025-06-27 02:18:04 +0700

    feat: Add widget statistics category in admin dashboard

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -5,12 +5,18 @@ namespace App\Filament\Widgets;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\Product;
+use App\Models\Category;
 
 class StatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
         return [
+            Stat::make('Total Kategori Produk', Category::count())
+                ->description('Jumlah semua kategori produk')
+                ->descriptionIcon('heroicon-m-shopping-bag')
+                ->color('warning'),
+
             Stat::make('Total Produk', Product::count())
                 ->description('Jumlah semua varian produk')
                 ->descriptionIcon('heroicon-m-shopping-bag')
