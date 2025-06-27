38cf68ccc4f18dc457d7a49811aba8f786ccef22
Drenzzz <realdrenzzz@gmail.com>
2025-06-27 09:19:00 +0700

    chore: Cleanup & Remove unused auth and layout files

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Di sini kita hanya mendefinisikan rute yang bisa diakses publik.
-| Rute untuk admin panel (/admin) diatur otomatis oleh Filament.
-|
-*/
-
-// Rute untuk halaman utama
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-// CONTOH: Jika nanti Anda ingin menambah halaman publik lain,
-// tambahkan di sini. Misalnya halaman "Tentang Kami".
-// Route::get('/tentang-kami', function () {
-//     return view('tentang');
-// })->name('about');
